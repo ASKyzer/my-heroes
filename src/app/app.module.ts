ca874f696caf47cf90f41eb441b8b8d4349c0692
Adrian Kyzer <adriankyzer@gmail.com>
2019-02-21 16:15:49 +0100

    combined filter funtionality to the hero component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FilterService } from './services/filter.service';
 import { HeroService } from './services/hero.service';
 import { DataService } from './services/data.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,17 +6,22 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    FilterComponent,
+    HeroesComponent,
   ],
   imports: [
     HttpModule,
+    FormsModule,
     BrowserModule
   ],
   providers: [
+    FilterService, 
     DataService,
     HeroService
   ],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { Http } from '@angular/http';
 import { HeroService } from './../services/hero.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,8 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
   heroes: any[]
+  character: string;
 
-  constructor(private service: HeroService) { 
+  constructor(private service: HeroService, private http: Http) { 
   }
 
   ngOnInit() {
@@ -17,8 +19,19 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes.data.results)
   }
 
+  filterSearch() {
+    console.log(this.character)
+    let url = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=" + this.character +  "&apikey=a2b97ce44d7dfdb3d3410ff2eeb8693b"
+    console.log(url)
+    this.http.get(url)
+      .subscribe(response => {
+        const data = response.json()
+        this.heroes = data.data.results
+        console.log(this.heroes)
+      })
+  }
+
   getImageUrl(hero) {
-    console.log(hero)
     const imgPath = hero.thumbnail.path
     const imgExtension = hero.thumbnail.extension
     return imgPath.concat('.', imgExtension)
