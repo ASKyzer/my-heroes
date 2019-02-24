d963f43e829df749700cc160e31aea5ff49c5b64
Adrian Kyzer <adriankyzer@gmail.com>
2019-02-24 20:29:37 +0100

    put filter back with hero component and set conditional to display either the all heroes results or the filtered heroes results base on whether the input field is empty or not.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { HeroService } from './../services/hero.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'heroes',
@@ -7,24 +7,22 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: any[]
   currentHeroes: any[]
-  character: string;
-  offset: number = 0;
+  selectedHero: any[]
+  character: string = ''
+  offset: number = 0
   finished = false
-  throttle = 100;
-  scrollDistance = 1;
-  scrollUpDistance = 2;
-  direction = '';
-  isOpen: boolean;
-
-  selectedHero: any
+  throttle = 100
+  scrollDistance = 1
+  scrollUpDistance = 2
+  direction = ''
+  isOpen: boolean
 
   constructor(private heroService: HeroService) { 
   }
 
-  onScrollDown (e) {
+  onAllScroll (e) {
     this.getMoreHeroes()
     this.direction = 'down'
   }
@@ -39,12 +37,50 @@ export class HeroesComponent implements OnInit {
       })
   }
 
+  filterSearch() {
+    this.offset = 0
+    if (this.character) {
+    console.log(this.character)
+    this.heroService.filterSearch(this.character)
+      .subscribe(response => {
+        const data = response.json()
+        this.heroes = data.data.results
+      })
+    }
+    else { this.ngOnInit() }
+  }
+
+  onFilterScroll () {
+    console.log('on filer scroll called')
+    this.getMoreFilterResults()
+    this.direction = 'down'
+  }
+  
+  getMoreFilterResults() {
+    this.offset += 50; 
+    this.heroService.getMoreFilterResults(this.character, this.offset)
+      .subscribe(response => {
+        const data = response.json()
+        const currentHeroes = data.data.results
+        this.concatHeroes(currentHeroes)
+      })
+  }
+
+
+  resetSearch() {
+    this.character = ''
+    this.offset = 0
+    console.log('clicked')
+    this.ngOnInit()
+  }
+
   concatHeroes(cur) {
     this.heroes = this.heroes.concat(cur)
   }
 
   ngOnInit() {
     this.isOpen = false;
+    this.character = ''
     this.heroService.getAll()
       .subscribe(response => {
         const data = response.json()
@@ -56,11 +92,9 @@ export class HeroesComponent implements OnInit {
     this.isOpen = eventArgs
   }
 
-  searchFieldPressed(eventArgs) {
-    console.log("search field Pressed")
-    console.log(eventArgs)
-    this.character = eventArgs
-  }
+  // searchFieldPressed(eventArgs) {
+  //   this.character = eventArgs
+  // }
 
   concatImageUrl(hero) {
     const imgPath = hero.thumbnail.path + "/standard_fantastic"
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -13,7 +13,22 @@ export class HeroService {
   }
     
   getMoreHeroes(offset) {
+    console.log(offset)
     let url = "https://gateway.marvel.com:443/v1/public/characters?limit=50&offset=" + offset + "&apikey=a2b97ce44d7dfdb3d3410ff2eeb8693b"
     return this.http.get(url)
   }
+
+  filterSearch(letters) {
+    console.log('filter search called')
+    console.log(letters)
+    let url: string = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=" + letters +  "&limit=50&apikey=a2b97ce44d7dfdb3d3410ff2eeb8693b"
+    return this.http.get(url);
+  }
+
+  getMoreFilterResults(letters, offset) {
+    console.log(letters, offset)
+    let url: string = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=" + letters +  "&limit=50&offset=" + offset + "&apikey=a2b97ce44d7dfdb3d3410ff2eeb8693b"
+    return this.http.get(url)
+  }
+
 }
